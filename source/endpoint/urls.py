ee0218d65df890e93b28bf1012aa7cd2094bd5ff
ABM1204 <melisovbz@gmail.com>
2024-12-19 00:31:51 +0600

    Для удобства добавил возможность перехеода на страницу со списком роботв через шаблон.

--- source/endpoint/urls.py
+++ source/endpoint/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from endpoint.views import get_all_robots
+from endpoint.views import get_all_robots, index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('robots/', get_all_robots, name='get_all_robots'),
 ]
--- source/endpoint/views.py
+++ source/endpoint/views.py
@@ -1,7 +1,10 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from robots.models import Robot
 
+def index(request):
+    return render(request, 'index.html')
 
 def get_all_robots(request):
     if request.method == "GET":
